dbfb58b84a63f2e4826e643bdd99bee586b6ba1f
MaxHalford <maxhalford25@gmail.com>
2018-05-04 20:10:55 +0200

    Use geometric mean for blending

--- submissions/blend.py
+++ submissions/blend.py
@@ -4,13 +4,13 @@ import glob
 
 files = glob.glob('*.csv')
 
-print(files[0])
 sub = pd.read_csv(files[0])
+sub['deal_probability'] = 1
 
-for f in files[1:]:
+for f in files:
     print(f)
-    sub['deal_probability'] += pd.read_csv(f)['deal_probability'].values
+    sub['deal_probability'] *= pd.read_csv(f)['deal_probability']
 
-sub['deal_probability'] = sub['deal_probability'] / len(files)
+sub['deal_probability'] = sub['deal_probability'] ** (1 / len(files))
 
 sub.to_csv('blend.csv', index=False)
